1598b41b9f65cdd47f3ed77d723f4cad5fb313ee
Riyiike <odusanyaiyanuoluwa@gmail.com>
2020-03-20 23:51:42 +0100

    error

--- signup.js
+++ signup.js
@@ -1,23 +1,35 @@
-const email = document.getElementById("email")
-const password = document.getElementById("password")
-const form = document.getElementById("form")
-const errorElement = document.getElementById("error")
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const username = document.getElementById('username');
+const confirm_password = document.getElementById('error');
 
-form.addEventListener("submit", (e) => {
-    let messages = []
-    if (email.value === '' || email.value == null) {
-        messages.push('Email is required')
-    }
+form.addEventListener('submit', e => {
+  let messages = [];
+  if (username.value === '' || username.value == null) {
+    username.style.border = '1px solid #5E6E66';
+    username_error.innerHTML = '';
+    return true;
+    /*window.alert('Please enter a username');
+         name.focus();
+         return false;*/
+  }
 
-    if (password.value.length <= 5) {
-        messages.push("password must be longer than 5 characters")
-    }
+  if (email.value === '' || email.value == null) {
+    messages.push('Email is required');
+    email.focus();
+    return false;
+  }
 
-    if (password.value.length > 12) {
-        messages.push("password must be less than 12 characters")
-    }
-    if (messages.length > 0) {
-        e.preventDefault()
-        errorElement.innerText = messages.join(', ')
-    }
-})
+  if (password.value.length <= 5) {
+    messages.push('password must be longer than 5 characters');
+    password.focus();
+    return false;
+  }
+
+  if (password.value.length > 12) {
+    messages.push('password must be less than 12 characters');
+    password.focus();
+    return false;
+  }
+  return true;
+});
